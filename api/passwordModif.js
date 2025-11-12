50209132a84c21d4211d1715b0be673428955d4f
Yannick <yannickkamdemkouam@yahoo.fr>
2025-11-12 23:18:46 +0100

    Attemp to fix cors problem to passwordModif route

--- api/passwordModif.js
+++ api/passwordModif.js
@@ -4,6 +4,12 @@ import connectDB from "../config/db.js";
 import User from "../models/User.js";
 
 export default async function handler(req, res) {
+
+    res.setHeader("Access-Control-Allow-Origin", process.env.KDM_GESTION_FRONT_URI);
+    res.setHeader("Access-Control-Allow-Methods", "POST,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    if (req.method === "OPTIONS") return res.status(200).end();
+
     // Vérifie que la méthode est POST
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Méthode non autorisée" });
